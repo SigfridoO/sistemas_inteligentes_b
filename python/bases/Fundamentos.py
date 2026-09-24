e59df3918e14c2ea69e75ec0d17940bde02f1bff
Sigfrido <sigfrido_o@ardilluda.com.mx>
2026-09-24 12:31:28 -0600

    iniciando python

---
+++ python/bases/Fundamentos.py
@@ -0,0 +1,65 @@
+print("hola mundo")
+
+# Esto es un comentario
+
+
+"""Este es un comentario
+ de 
+ muchas 
+ lineas"""
+
+# =========================== variables  ===========================
+#int:
+edad = 21
+#float:
+estatura = 1.50
+#str:
+nombre = "Rodolfo"
+#bool:
+fuma = False
+
+
+print("edad:",edad)
+print("estatura:",estatura)
+print("nombre:",nombre)
+print("fuma:",fuma)
+
+# =========================== operadores aritméticos  ===========================
+a = 13
+b = 3
+
+print("a:", a)
+print("b:", b)
+
+print ("a + b: ", a + b)
+print ("a - b: ", a - b)
+print ("a * b: ", a * b)
+print ("a / b: ", a / b)
+print ("a % b: ", a % b)
+print ("a ** b: ", a ** b)
+
+
+# =========================== operadores de comparación  ===========================
+
+numero_1 = 5
+numero_2 = 6
+
+print("numero_1:", numero_1)
+print("numero_2:", numero_2)
+
+print("numero_1 > numero_2 :", numero_1 > numero_2)
+print("numero_1 < numero_2 :", numero_1 < numero_2)
+print("numero_1 >= numero_2 :", numero_1 >= numero_2)
+print("numero_1 <= numero_2 :", numero_1 <= numero_2)
+print("numero_1 == numero_2 :", numero_1 == numero_2)
+
+# =========================== operadores boleanos  ===========================
+
+x = True
+y = False;
+
+print("x: ", x)
+print("y: ", y)
+print("x or y:", x or y)
+print("x and y:", x and y)
+print("not x", not x)
